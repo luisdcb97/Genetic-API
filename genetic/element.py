417fe5d246aa4256c9ed0270b90548e1377b31c2
Luis David <luisdcb97@gmail.com>
2017-12-31 21:17:02 +0000

    Add position, velocity and acceleration to elements

--- genetic/element.py
+++ genetic/element.py
@@ -2,5 +2,17 @@ from physics.vector import Vector, Number
 
 
 class Element(object):
-    def __init__(self):
-        self.x = Vector(1, 2)
+    def __init__(self, position: Vector=None, velocity: Vector=None):
+        self.position: Vector = position \
+            if position is not None else Vector.random()
+        self.velocity: Vector = velocity \
+            if velocity is not None else Vector.random()
+        self.acceleration: Vector = Vector.zero()
+
+    def apply_force(self, force: Vector):
+        self.acceleration += force
+
+    def update(self):
+        self.velocity += self.acceleration
+        self.position += self.velocity
+        self.acceleration *= 0
